7dc8c45b046455f37084ea6da5ca45a963356dd5
EduardoALima1984 <ealima27@gmail.com>
2025-08-30 18:54:25 -0300

    inclusão de soma, correção d e alguns Bugs, implantação do botão soma

--- MauiApp1MINHASCOMPRAS/Visuais/ListaProduto.xaml.cs
+++ MauiApp1MINHASCOMPRAS/Visuais/ListaProduto.xaml.cs
@@ -5,9 +5,28 @@ using MauiApp1MINHASCOMPRAS.ajudadores;
 namespace MauiApp1MINHASCOMPRAS.Visuais;
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
     public ListaProduto()
-    { InitializeComponent();
+    { 
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
     }
+
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.GetAll();
+
+        tmp.ForEach(i => lista.Add(i));
+    
+    }
+
     private async void ToolbarItem_Clicked(object sender, EventArgs e)
     {
         try
@@ -17,5 +36,25 @@ public partial class ListaProduto : ContentPage
         {
             await DisplayAlert("OPS", ex.Message, "OK");
         }
+    }
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total Dos Produtos", msg, "OK");
+
     }
 }
